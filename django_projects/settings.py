fe4302f172571810ab22acf6921f2547a0dce480
Oni Olalekan <olalekan.joseph@gmail.com>
2019-02-25 12:46:43 +0100

    SSH TESTING

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -24,6 +24,8 @@ SECRET_KEY = '0%$v8&)*te+8ac6)1his4@a#x#g4)1_z+&&glc+l4_qrqzj^ug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # I need to change this debug true in production. I will do that just calm down.
+# We need a viable opposition to the two leading political parties in Nigeria.
+# It's important for the electorates to vote and ensure that their votes counts
 DEBUG = True
 
 ALLOWED_HOSTS = ['electionblog.herokuapp.com']
